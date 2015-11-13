757c7da97ce1bddae13d6fd58af5329bc52c5ded
Brunixrogue <brunixrogue@gmail.com>
2015-11-13 09:12:26 -0800

    New

---
+++ Game World/Assets/PC2D/Scripts/SoundCOntroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundCOntroller : MonoBehaviour {
+    public AudioSource audio;
+    public AudioClip jumpSound;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown(PC2D.Input.JUMP))
+        audio.PlayOneShot(jumpSound);
+
+    }
+}
---
+++ Game World/Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public Text scoreText;
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        UpdateScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+
+}
+
+
+
